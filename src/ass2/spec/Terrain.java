0ec156a4d3da4c5e3bb99f4be9291336247fc577
Wemyss <wemyss@users.noreply.github.com>
2016-10-22 16:56:28 +1100

    Tmp camera (#2)
    * merged in master
    
    * camera doesnt work in 3rd person
    
    * temp avatar commit
    
    * made third person view work

--- src/ass2/spec/Terrain.java
+++ src/ass2/spec/Terrain.java
@@ -245,6 +245,7 @@ public class Terrain {
     }
 
 	public void drawTerrain(GL2 gl, Texture[] textures) {
+		gl.glPushMatrix();
 		gl.glBindTexture(GL2.GL_TEXTURE_2D, textures[SAND].getTextureId());
 		
 //		gl.glColor4d(0, 0, 0, 1); // color
@@ -295,6 +296,7 @@ public class Terrain {
 //        	}
 //        }
 //        gl.glEnd();
+        gl.glPopMatrix();
 	}
 
 	public void drawTrees(GL2 gl, Texture[] textures) {
